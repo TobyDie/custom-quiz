c5d4387342de1c1488fd776abc55ae4365148803
TobyDie <53049174+TobyDie@users.noreply.github.com>
2023-04-05 13:56:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,57 +17,48 @@ $(document).ready(function() {
 
     // Show submit button if last question has been answered
     if (nextQuestion.hasClass('form')) {
-      $('#submit-btn').show();
+      $('.submit-btn').show();
     }
   });
-
-  // Hide submit button initially
-  $('#submit-btn').hide();
 
   // Handle form submission
   $('form').on('submit', function(e) {
     e.preventDefault();
 
     // Get user's answers
-    var answers = [];
+    var answers = {};
     $('.question').each(function() {
-      var questionText = $(this).find('.question-text').text();
-      var answerText = $(this).find('label.selected').text();
-      answers.push({
-        question: questionText,
-        answer: answerText
-      });
+      var questionId = $(this).data('question-id');
+      var answerId = $(this).find('input[type="radio"]:checked').attr('id');
+      answers[questionId] = answerId;
     });
 
     // Get user's name and email
     var name = $('#name').val();
     var email = $('#email').val();
 
-    // Save user's answers, name, and email to cookie and local storage
-    var quizData = {
+    // Save user's answers, name, and email to cookie with 90 day expiry
+    var data = {
       answers: answers,
       name: name,
       email: email
     };
-    Cookies.set('quiz_data', JSON.stringify(quizData), { expires: 90, domain: '.hairqare.co' });
-    localStorage.setItem('quiz_data', JSON.stringify(quizData));
+    document.cookie = "quiz_data=" + JSON.stringify(data) + ";max-age=7776000;path=/;domain=.hairqare.co";
 
     // Post user's answers, name, and email to webhook
-    $.post('https://hook.us1.make.com/7ldadddexettepgl3ftl7beuu3i8cp4t', quizData)
-      .done(function() {
-        console.log('Data posted to webhook');
-      })
-      .fail(function() {
-        console.log('Failed to post data to webhook');
-      })
-      .always(function() {
+    $.ajax({
+      url: 'https://hook.us1.make.com/7ldadddexettepgl3ftl7beuu3i8cp4t',
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function() {
         // Redirect to thank you page
         window.location.href = 'https://checkout.hairqare.co/buy/hairqare-challenge-save-90/';
-      });
-  });
-
-  // Add loading animation to submit button
-  $('#submit-btn').on('click', function() {
-    $(this).addClass('loading');
+      },
+      error: function() {
+        // Redirect to thank you page
+        window.location.href = 'https://checkout.hairqare.co/buy/hairqare-challenge-save-90/';
+      }
+    });
   });
 });
