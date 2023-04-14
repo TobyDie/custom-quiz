d443256916e85f6787343013cdd94b64b15cd697
TobyDie <53049174+TobyDie@users.noreply.github.com>
2023-04-14 11:15:59 +0800

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,5 @@
+<!-- Start Converge Pixel Snippet -->
+
+!function(e,n,i,t,_,s){e.cvg||("shopify"in e||"undefined"!=typeof analytics&&"undefined"!=typeof browser&&"undefined"!=typeof init&&(e.shopify={},e.shopify.analytics=analytics,e.shopify.browser=browser,e.shopify.init=init),(t=e.cvg=function(){t.process?t.process.apply(t,arguments):t.queue.push(arguments)}).queue=[],t.t=+new Date,(_=n.createElement(i)).async=1,_.src="https://static.runconverge.com/convergepixelV3.min.js",(s=n.getElementsByTagName(i)[0]).parentNode.insertBefore(_,s))}(window,document,"script"),cvg({method:"init",id:'WcSW9c',params:{shopName:'undefined',ga4MeasurementId:'G-VS1R24SPVN',enableCriteo:void 0,send_fb_pageview:true}});
+
+<!-- End Converge Pixel Snippet -->
