3c5113a58c0989623251364d4017e38c78875e31
TobyDie <53049174+TobyDie@users.noreply.github.com>
2023-04-14 11:24:02 +0800

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,5 +1,13 @@
 <!-- Start Converge Pixel Snippet -->
-
-!function(e,n,i,t,_,s){e.cvg||("shopify"in e||"undefined"!=typeof analytics&&"undefined"!=typeof browser&&"undefined"!=typeof init&&(e.shopify={},e.shopify.analytics=analytics,e.shopify.browser=browser,e.shopify.init=init),(t=e.cvg=function(){t.process?t.process.apply(t,arguments):t.queue.push(arguments)}).queue=[],t.t=+new Date,(_=n.createElement(i)).async=1,_.src="https://static.runconverge.com/convergepixelV3.min.js",(s=n.getElementsByTagName(i)[0]).parentNode.insertBefore(_,s))}(window,document,"script"),cvg({method:"init",id:'WcSW9c',params:{shopName:'undefined',ga4MeasurementId:'G-VS1R24SPVN',enableCriteo:void 0,send_fb_pageview:true}});
-
+<script type="text/javascript">
+    !function(e,n,i,t,s,o){e.cvg||((t=e.cvg=function(){t.process?
+    t.process.apply(t,arguments):t.queue.push(arguments)}).queue=[],
+    t.t=+new Date,(s=n.createElement(i)).async=1,
+    s.src="https://static.runconverge.com/convergepixelV3.js",
+    (o=n.getElementsByTagName(i)[0]).parentNode.insertBefore(s,o))}
+    (window,document,"script"),cvg({method:"init",id:"WcSW9c",params:{}});
+    cvg({method:"event",event:"$page_load"});
+ </script>
 <!-- End Converge Pixel Snippet -->
+
+
